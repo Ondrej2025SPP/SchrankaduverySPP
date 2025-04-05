c7068609d86980dd7cd4b03d856891d06d71a7e6
Ondrej2025SPP <ondrakubesbartu@seznam.cz>
2025-04-05 20:53:35 +0200

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,50 +1,38 @@
+const password = "schrankaduverySPP2025";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase, ref, onValue, update } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
-
-// Firebase konfigurace – ověřená dle uživatelem zaslaného screenshotu
-const firebaseConfig = {
-  apiKey: "AIzaSyByfibBg6OmQn6TfM40Dj6Vo6fKUhgn5Ls",
-  authDomain: "schranka-duvery-spp.firebaseapp.com",
-  databaseURL: "https://schranka-duvery-spp-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "schranka-duvery-spp",
-  storageBucket: "schranka-duvery-spp.appspot.com",
-  messagingSenderId: "357685746373",
-  appId: "1:357685746373:web:06c96b84c8043310e86eac",
-  measurementId: "G-1FL2RT5EPZ"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-const messagesRef = ref(db, "messages");
-
-onValue(messagesRef, (snapshot) => {
-  const data = snapshot.val();
-  const container = document.getElementById("adminMessages");
-  container.innerHTML = "";
-
-  if (data) {
-    Object.entries(data).forEach(([code, obj]) => {
-      const div = document.createElement("div");
-      div.className = "message";
-      div.innerHTML = `
-        <p><strong>Kód:</strong> ${code}</p>
-        <p><strong>Zpráva:</strong> ${obj.message}</p>
-        <p><strong>Odpověď:</strong> ${obj.response || "zatím žádná"}</p>
-        <textarea id="response-${code}" rows="2" placeholder="Napiš odpověď...">${obj.response || ""}</textarea>
-        <button onclick="sendResponse('${code}')">Odeslat odpověď</button>
-        <hr>
-      `;
-      container.appendChild(div);
-    });
+function login() {
+  const input = document.getElementById("adminPass").value;
+  if (input === password) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("adminPanel").style.display = "block";
+    loadMessages();
   } else {
-    container.innerHTML = "<p>Žádné zprávy zatím nejsou.</p>";
+    alert("Nesprávné heslo.");
   }
-});
+}
 
-window.sendResponse = function(code) {
-  const responseText = document.getElementById("response-" + code).value;
-  const messageRef = ref(db, "messages/" + code);
-  update(messageRef, { response: responseText });
-};
+function loadMessages() {
+  const messages = JSON.parse(localStorage.getItem("messages") || "{}");
+  const list = document.getElementById("messageList");
+  list.innerHTML = "";
+
+  Object.keys(messages).forEach(code => {
+    const li = document.createElement("li");
+    li.innerHTML = `<strong>Kód:</strong> ${code}<br><strong>Zpráva:</strong> ${messages[code].text}<br>
+    <strong>Odpověď:</strong> <input data-code="${code}" value="${messages[code].reply || ""}" />
+    <button onclick="saveReply(this)">Uložit</button>`;
+    list.appendChild(li);
+  });
+}
+
+function saveReply(button) {
+  const input = button.previousElementSibling;
+  const code = input.getAttribute("data-code");
+  const messages = JSON.parse(localStorage.getItem("messages") || "{}");
+
+  if (messages[code]) {
+    messages[code].reply = input.value;
+    localStorage.setItem("messages", JSON.stringify(messages));
+    alert("Odpověď uložena.");
+  }
+}
